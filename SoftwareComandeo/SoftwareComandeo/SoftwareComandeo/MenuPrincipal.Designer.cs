304a8df6c1189f1b01ab9040793ad325b79b4aa4
SergioEduardoEsLa <sergio.eduardo.esla@outlook.com>
2018-11-11 14:46:49 -0600

    Iconos Agregados
    En este comit ya se pudo ver las imagenes de los botones.
    El diseño recomendado para todas las pantallas es:
    Fuente Courrier New en negrita.
    Background color: Web/CadetBlue

--- SoftwareComandeo/SoftwareComandeo/SoftwareComandeo/MenuPrincipal.Designer.cs
+++ SoftwareComandeo/SoftwareComandeo/SoftwareComandeo/MenuPrincipal.Designer.cs
@@ -36,7 +36,8 @@
             // 
             // btnCerrarSesionMenu
             // 
-            this.btnCerrarSesionMenu.Location = new System.Drawing.Point(161, 337);
+            this.btnCerrarSesionMenu.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrarSesionMenu.Location = new System.Drawing.Point(195, 341);
             this.btnCerrarSesionMenu.Name = "btnCerrarSesionMenu";
             this.btnCerrarSesionMenu.Size = new System.Drawing.Size(192, 68);
             this.btnCerrarSesionMenu.TabIndex = 2;
@@ -46,12 +47,14 @@
             // btnCorteDeCaja
             // 
             this.btnCorteDeCaja.AutoSize = true;
-            this.btnCorteDeCaja.Image = ((System.Drawing.Image)(resources.GetObject("btnCorteDeCaja.Image")));
+            this.btnCorteDeCaja.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCorteDeCaja.BackgroundImage")));
+            this.btnCorteDeCaja.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCorteDeCaja.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCorteDeCaja.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnCorteDeCaja.ImageKey = "(ninguno)";
-            this.btnCorteDeCaja.Location = new System.Drawing.Point(274, 40);
+            this.btnCorteDeCaja.Location = new System.Drawing.Point(274, 15);
             this.btnCorteDeCaja.Name = "btnCorteDeCaja";
-            this.btnCorteDeCaja.Size = new System.Drawing.Size(231, 276);
+            this.btnCorteDeCaja.Size = new System.Drawing.Size(262, 320);
             this.btnCorteDeCaja.TabIndex = 1;
             this.btnCorteDeCaja.Text = "CORTE DE CAJA";
             this.btnCorteDeCaja.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -60,12 +63,14 @@
             // btnComanda
             // 
             this.btnComanda.AutoSize = true;
-            this.btnComanda.Image = ((System.Drawing.Image)(resources.GetObject("btnComanda.Image")));
+            this.btnComanda.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnComanda.BackgroundImage")));
+            this.btnComanda.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnComanda.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnComanda.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnComanda.ImageKey = "(ninguno)";
-            this.btnComanda.Location = new System.Drawing.Point(12, 40);
+            this.btnComanda.Location = new System.Drawing.Point(12, 15);
             this.btnComanda.Name = "btnComanda";
-            this.btnComanda.Size = new System.Drawing.Size(231, 276);
+            this.btnComanda.Size = new System.Drawing.Size(262, 320);
             this.btnComanda.TabIndex = 0;
             this.btnComanda.Text = "COMANDA";
             this.btnComanda.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -76,12 +81,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(517, 421);
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(548, 412);
             this.Controls.Add(this.btnCerrarSesionMenu);
             this.Controls.Add(this.btnCorteDeCaja);
             this.Controls.Add(this.btnComanda);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MENU PRINCIPAL";
             this.ResumeLayout(false);
             this.PerformLayout();
